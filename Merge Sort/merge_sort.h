1eb7e16f175f38c8e34eabbd0ac8aae8d46da83d
Vinícius Ferreira Aguiar <40642605+AceVinicius@users.noreply.github.com>
2019-08-21 17:59:53 -0300

    Update merge_sort.h

--- Merge Sort/merge_sort.h
+++ Merge Sort/merge_sort.h
@@ -5,17 +5,17 @@
 
 void
 mergeSort(      char *user_v_,
-          const int   begin_,
-          const int   end_);
+          const int   begin_ ,
+          const int   end_   );
 
 
 
 void
-merge(      char *user_v_,
+merge(      char *user_v_ ,
       const int   l_begin_,
-      const int   l_end_,
+      const int   l_end_  ,
       const int   r_begin_,
-            int   r_end_);
+            int   r_end_  );
 
 
 
